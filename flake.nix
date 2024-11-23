ab6bba343c596fbec6133e612194a44de81747c6
Rom Rakharb <rom.rakharb@mailfence.com>
2024-11-23 22:34:13 +0700

    complete setup

--- flake.nix
+++ flake.nix
@@ -8,35 +8,28 @@
   outputs = { self, nixpkgs }:
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    target = "thumbv7em-none-eabihf";
   in
   {
     devShells.x86_64-linux.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         rustup
-        # gcc-arm-embedded
-        # cargo-binutils
-        # probe-rs-tools
-        # minicom
+        cargo-binutils
+        probe-rs-tools
+        gcc-arm-embedded-13
       ];
+
 
       shellHook = ''
         rustup default stable
-        rustup add component rust-analyzer
+        rustup component add rust-analyzer llvm-tools
+        rustup target add ${target}
+        echo -e '[language-server.rust-analyzer.config]\ncheck.command = "clippy"\ncheck.allTargets = false\ncargo.target = "${target}"' > ~/.config/helix/languages.toml
+        echo -e '[build]\ntarget = "${target}"\n\n[target.${target}]\nrustflags = ["-C", "link-arg=-Tlink.x"]' > ./.cargo/config.toml
         rustup update
         clear
-        # echo -e "# CMSIS-DAP for microbit\n\nACTION!=\"add|change\", GOTO=\"microbit_rules_end\"\n\nSUBSYSTEM==\"usb\", ATTR{idVendor}==\"0d28\", ATTR{idProduct}==\"0204\", TAG+=\"uaccess\"\n\nLABEL=\"microbit_rules_end\"" | sudo tee /etc/udev/rules.d/69-microbit.rules > /dev/null
-        # sudo udevadm control --reload
-        # sudo udevadm trigger
-        # clear
-        # echo "Development Environment for Embedded Rust"
-        # echo "-------------------------------------------------"
-        # rustc --version
-        # echo "-------------------------------------------------"
-        # lsusb | grep -i "NXP ARM mbed" | awk '{print "/dev/bus/usb/" $2 "/" $4}' | sed 's/://g' | xargs -I {} sh -c 'ls -l {}; echo "-------------------------------------------------"; getfacl {}'
-        # echo "-------------------------------------------------"
-        # probe-rs list
-        # rustup target add thumbv7em-none-eabihf
-        # cargo embed --target thumbv7em-none-eabihf
+        echo "Development Environment for Embedded Rust"
+        rustup show
       '';
     };
   };
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cortex_m::asm::nop;
+use cortex_m_rt::entry;
+use panic_halt as _;
+// use rtt_target::{rprintln, rtt_init_print};
+
+#[entry]
+fn main() -> ! {
+    let mut x = 0;
+    loop {
+        x += 1;
+        for _ in 0..x {
+            nop();
+        }
+    }
+    // rtt_init_print!();
+    // rprintln!("Hello, world!");
+    // loop {
+    //     rprintln!("Echo..");
+    //     for _ in 0..100_000 {
+    //         nop();
+    //     }
+    // }
 }
